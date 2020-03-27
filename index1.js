f67d58fe5627fed6e805b6a0263d2eb86ed4433e
Samia Islam Oyse <60336691+samiaislam16@users.noreply.github.com>
2020-03-27 21:43:18 +0600

    Add files via upload

---
+++ index1.js
@@ -0,0 +1,13 @@
+document.getElementsById("clickbtn"). addEventListener("click",dbblclick1);
+
+function dbblclick1()
+{
+	document.getElementsById("demo1"). setAttribute("style","display:block;opacity:1;transform:scaley(1)";
+}
+document.getElementsById("clickbtn2"). addEventListener("click",dbblclick2);
+
+
+function dbblclick2()
+{
+	document.getElementsById("demo1"). setAttribute("style","opacity:0;transform:scaley(0)");
+}
